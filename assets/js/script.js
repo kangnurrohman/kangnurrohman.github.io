2f2b91e5153bd29366f3f321b2c005dde1deee3d
kangnurrohman <ainurrohman869@gmail.com>
2023-06-30 09:18:35 +0700

    update blog

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,3 +10,34 @@ function toggleNightMode(){
 		localStorage.setItem("theme","");
 	}
 }
+
+window.addEventListener('scroll', function() {
+      var backToTop = document.getElementById('backToTop');
+      if (window.pageYOffset > 100) {
+        backToTop.style.display = 'block';
+      } else {
+        backToTop.style.display = 'none';
+      }
+    });
+
+    function scrollToTop() {
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+      });
+    }
+
+    function updateButtonBackground() {
+      var body = document.body;
+      var backToTop = document.getElementById('backToTop');
+      if (getComputedStyle(body).getPropertyValue('background-color') === 'rgb(0, 0, 0)') {
+        backToTop.style.backgroundColor = 'white';
+        backToTop.style.color = 'black';
+      } else {
+        backToTop.style.backgroundColor = 'black';
+        backToTop.style.color = 'white';
+      }
+    }
+
+    window.addEventListener('DOMContentLoaded', updateButtonBackground);
+    window.addEventListener('resize', updateButtonBackground);
