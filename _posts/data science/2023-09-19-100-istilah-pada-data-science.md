a5963dc53a8fa5e73cceaa9223848bfcbbf3046a
kangnurrohman <ainurrohman869@gmail.com>
2023-09-19 09:36:23 +0700

    update

---
+++ _posts/data science/2023-09-19-100-istilah-pada-data-science.md
@@ -0,0 +1,30 @@
+---
+title: "100 Istilah Pada Data Science"
+date: 2023-09-19T00:00:00+00:00
+update: 2023-09-19T00:00:00+00:00
+author: Kang Nur
+layout: post
+permalink: /100-istilah-pada-data-science/
+image: ""
+description: ""
+categories: Data Science
+tags: [analisis data, data science]
+published: true
+---
+
+<table id="myDataTable">
+    <thead>
+        <tr>
+            <th>Nama</th>
+            <th>Usia</th>
+            <!-- Tambahkan lebih banyak kolom di sini sesuai kebutuhan Anda -->
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>John Doe</td>
+            <td>30</td>
+        </tr>
+        <!-- Tambahkan lebih banyak baris di sini -->
+    </tbody>
+</table>
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,85 +1,87 @@
 // switch dark mode
-function toggleNightMode(){
-	if(document.documentElement.getAttribute('data-theme') == 'light'){
-		document.documentElement.setAttribute('data-theme', 'dark');
-		document.getElementById('mode-switcher').classList.add('active');
-		localStorage.setItem("theme","dark");
-	}
-	else{
-		document.documentElement.setAttribute('data-theme', 'light');
-		document.getElementById('mode-switcher').classList.remove('active');
-		localStorage.setItem("theme","");
-	}
+function toggleNightMode() {
+  if (document.documentElement.getAttribute("data-theme") == "light") {
+    document.documentElement.setAttribute("data-theme", "dark");
+    document.getElementById("mode-switcher").classList.add("active");
+    localStorage.setItem("theme", "dark");
+  } else {
+    document.documentElement.setAttribute("data-theme", "light");
+    document.getElementById("mode-switcher").classList.remove("active");
+    localStorage.setItem("theme", "");
+  }
 }
 
 // back to top
-window.addEventListener('scroll', function() {
-      var backToTop = document.getElementById('backToTop');
-      if (window.pageYOffset > 100) {
-        backToTop.style.display = 'block';
-      } else {
-        backToTop.style.display = 'none';
-      }
-    });
+window.addEventListener("scroll", function () {
+  var backToTop = document.getElementById("backToTop");
+  if (window.pageYOffset > 100) {
+    backToTop.style.display = "block";
+  } else {
+    backToTop.style.display = "none";
+  }
+});
 
-    function scrollToTop() {
-      window.scrollTo({
-        top: 0,
-        behavior: 'smooth'
-      });
-    }
+function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+}
 
 function updateButtonBackground() {
-      var body = document.body;
-      var backToTop = document.getElementById('backToTop');
-      if (getComputedStyle(body).getPropertyValue('background-color') === 'rgb(16, 23, 30)') {
-        backToTop.style.backgroundColor = 'white';
-        backToTop.style.color = 'black';
-      } else {
-        backToTop.style.backgroundColor = 'rgb(16, 23, 30)';
-        backToTop.style.color = 'rgb(191, 191, 191)';
-      }
-    }
+  var body = document.body;
+  var backToTop = document.getElementById("backToTop");
+  if (
+    getComputedStyle(body).getPropertyValue("background-color") ===
+    "rgb(16, 23, 30)"
+  ) {
+    backToTop.style.backgroundColor = "white";
+    backToTop.style.color = "black";
+  } else {
+    backToTop.style.backgroundColor = "rgb(16, 23, 30)";
+    backToTop.style.color = "rgb(191, 191, 191)";
+  }
+}
 
-    window.addEventListener('DOMContentLoaded', updateButtonBackground);
-    window.addEventListener('resize', updateButtonBackground);
+window.addEventListener("DOMContentLoaded", updateButtonBackground);
+window.addEventListener("resize", updateButtonBackground);
 
 // collapse navbar
 function toggleContent(id) {
-      var element = document.getElementById(id);
-      element.classList.toggle("hidden");
-      var arrow = document.getElementById("arrow-" + id);
-      arrow.classList.toggle("collapsed");
-    }
+  var element = document.getElementById(id);
+  element.classList.toggle("hidden");
+  var arrow = document.getElementById("arrow-" + id);
+  arrow.classList.toggle("collapsed");
+}
 
 // view count
 
 // Estimate Reading Time
-  document.addEventListener("DOMContentLoaded", function() {
-    var content = document.getElementsByClassName("blog-post-content")[0];
-    var words = content.innerText.split(' ').length;
-    var readingTime = Math.ceil(words / 200); // Estimasi waktu membaca dengan kecepatan 200 kata per menit
-    var readingTimeText = readingTime + " menit membaca";
+document.addEventListener("DOMContentLoaded", function () {
+  var content = document.getElementsByClassName("blog-post-content")[0];
+  var words = content.innerText.split(" ").length;
+  var readingTime = Math.ceil(words / 200); // Estimasi waktu membaca dengan kecepatan 200 kata per menit
+  var readingTimeText = readingTime + " menit membaca";
 
-    var readingTimeElement = document.getElementById("reading-time");
-    readingTimeElement.innerText = readingTimeText;
-  });
+  var readingTimeElement = document.getElementById("reading-time");
+  readingTimeElement.innerText = readingTimeText;
+});
 
 // highlight code
-document.querySelectorAll('.copy-button').forEach(button => {
-  button.addEventListener('click', copyToClipboard);
+document.querySelectorAll(".copy-button").forEach((button) => {
+  button.addEventListener("click", copyToClipboard);
 });
 
 function copyToClipboard(event) {
   const button = event.target;
-  const codeContainer = button.closest('.code-container');
-  const codeElement = codeContainer.querySelector('code');
+  const codeContainer = button.closest(".code-container");
+  const codeElement = codeContainer.querySelector("code");
   const codeText = codeElement.textContent.trim();
-  const textarea = document.createElement('textarea');
+  const textarea = document.createElement("textarea");
   textarea.value = codeText;
   document.body.appendChild(textarea);
   textarea.select();
-  document.execCommand('copy');
+  document.execCommand("copy");
   document.body.removeChild(textarea);
   button.innerHTML = '<i class="fas fa-check"></i> Disalin!';
   setTimeout(() => {
@@ -87,3 +89,13 @@ function copyToClipboard(event) {
   }, 2000);
 }
 
+// Datatable
+
+$(document).ready(function () {
+  $("#myDataTable").DataTable({
+    pageLength: 10,
+    searching: true,
+    dom: "Bfrtip", // Menambahkan tombol eksport (opsional)
+    buttons: ["copy", "csv", "excel", "pdf", "print"], // Tombol eksport (opsional)
+  });
+});
